b75ed2e9b645bdfcd037a0d585f2ad0170433821
ilya <rinekri@gmail.com>
2017-04-08 23:22:42 +0200

    Add logic to show API KEY in MainActivity

--- app/src/main/java/ru/rinekri/udacitypopularmovies/ui/main/MainActivity.java
+++ app/src/main/java/ru/rinekri/udacitypopularmovies/ui/main/MainActivity.java
@@ -1,14 +1,21 @@
 package ru.rinekri.udacitypopularmovies.ui.main;
 
+import android.widget.TextView;
+
 import com.arellomobile.mvp.presenter.InjectPresenter;
 import com.arellomobile.mvp.presenter.ProvidePresenter;
 
+import butterknife.BindView;
+import ru.rinekri.udacitypopularmovies.BuildConfig;
 import ru.rinekri.udacitypopularmovies.R;
 import ru.rinekri.udacitypopularmovies.network.models.Foo;
 import ru.rinekri.udacitypopularmovies.ui.base.ActivityConfig;
 import ru.rinekri.udacitypopularmovies.ui.base.BaseMvpActivity;
 
 public class MainActivity extends BaseMvpActivity<Foo> implements MainView {
+
+  @BindView(R.id.text_view)
+  TextView textView;
 
   @InjectPresenter
   public MainPresenter presenter;
@@ -28,5 +35,6 @@ public class MainActivity extends BaseMvpActivity<Foo> implements MainView {
 
   @Override
   protected void initView() {
+    textView.setText(BuildConfig.DB_MOVIE_API_KEY);
   }
 }
