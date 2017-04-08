d4ccd6c670b3d4b3d2c5f1838b0b4b5877fe6681
ilya <rinekri@gmail.com>
2017-04-08 18:43:40 +0200

    Add retrolambda and some pachage changes

--- app/src/main/java/ru/rinekri/udacitypopularmovies/ui/main/MainActivity.java
+++ app/src/main/java/ru/rinekri/udacitypopularmovies/ui/main/MainActivity.java
@@ -1,7 +1,9 @@
-package ru.rinekri.udacitypopularmovies;
+package ru.rinekri.udacitypopularmovies.ui.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import ru.rinekri.udacitypopularmovies.R;
 
 public class MainActivity extends AppCompatActivity {
 
