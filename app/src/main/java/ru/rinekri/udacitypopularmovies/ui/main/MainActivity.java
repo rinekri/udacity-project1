36e105266d61faa4af6fa7e0ed2ba399bf2fb839
ilya <rinekri@gmail.com>
2017-04-09 12:29:14 +0200

    Add logic to show content

--- app/src/main/java/ru/rinekri/udacitypopularmovies/ui/main/MainActivity.java
+++ app/src/main/java/ru/rinekri/udacitypopularmovies/ui/main/MainActivity.java
@@ -1,18 +1,24 @@
 package ru.rinekri.udacitypopularmovies.ui.main;
 
-import android.widget.TextView;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.arellomobile.mvp.presenter.InjectPresenter;
 import com.arellomobile.mvp.presenter.ProvidePresenter;
 
 import butterknife.BindView;
 import ru.rinekri.udacitypopularmovies.R;
+import ru.rinekri.udacitypopularmovies.network.models.MovieInfo;
 import ru.rinekri.udacitypopularmovies.ui.base.ActivityConfig;
 import ru.rinekri.udacitypopularmovies.ui.base.BaseMvpActivity;
 
+import static ru.rinekri.udacitypopularmovies.ui.UiConstants.GRID_COLUMNS;
+
 public class MainActivity extends BaseMvpActivity<MainPM> implements MainView {
-  @BindView(R.id.text_view)
-  TextView recyclerView;
+  @BindView(R.id.main_content_view)
+  RecyclerView contentView;
+
+  MainAdapter<MovieInfo> contentAdapter;
 
   @InjectPresenter
   public MainPresenter presenter;
@@ -31,8 +37,16 @@ public class MainActivity extends BaseMvpActivity<MainPM> implements MainView {
   }
 
   @Override
+  protected void initView() {
+    contentAdapter = new MainAdapter<>();
+    contentView.setAdapter(contentAdapter);
+    RecyclerView.LayoutManager manager = new GridLayoutManager(this, GRID_COLUMNS);
+    contentView.setLayoutManager(manager);
+  }
+
+  @Override
   public void showContent(MainPM data) {
     super.showContent(data);
-
+    contentAdapter.swapContent(data.movies());
   }
 }
