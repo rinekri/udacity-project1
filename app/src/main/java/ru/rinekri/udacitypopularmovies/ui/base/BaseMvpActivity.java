f30451bdd6b111fbff533029b9ffa3f1d2964824
ilya <rinekri@gmail.com>
2017-04-11 08:03:57 +0200

    Add base logic to manage ELCE states

--- app/src/main/java/ru/rinekri/udacitypopularmovies/ui/base/BaseMvpActivity.java
+++ app/src/main/java/ru/rinekri/udacitypopularmovies/ui/base/BaseMvpActivity.java
@@ -3,16 +3,24 @@ package ru.rinekri.udacitypopularmovies.ui.base;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
 
 import butterknife.ButterKnife;
 import ru.rinekri.udacitypopularmovies.R;
 
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
 abstract public class BaseMvpActivity<D> extends MvpAppCompatActivity implements BaseMvpView<D> {
 
   protected abstract ActivityConfig provideActivityConfig();
   protected void initView() {}
+
+  private View errorView;
+  private View emptyView;
+  private View progressView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +28,18 @@ abstract public class BaseMvpActivity<D> extends MvpAppCompatActivity implements
     ActivityConfig config = provideActivityConfig();
 
     setContentView(config.contentRes());
-    initActionBar(config);
     ButterKnife.bind(this);
+    initActionBar(config);
+    initElceViews(config);
     initView();
+  }
+
+  private void initElceViews(ActivityConfig config) {
+    emptyView = ButterKnife.findById(this, config.emptyViewId());
+    errorView = ButterKnife.findById(this, config.errorViewId());
+    progressView = ButterKnife.findById(this, config.progressViewId());
+
+    //TODO: Add logic to correctly manage ELCE states with content
   }
 
   private void initActionBar(ActivityConfig config) {
@@ -40,16 +57,31 @@ abstract public class BaseMvpActivity<D> extends MvpAppCompatActivity implements
     ab.setDisplayHomeAsUpEnabled(config.useBackButton());
   }
 
-  //TODO: Add logic to manage ELCE states
   @Override
-  public void showContent(D data) {}
+  public void showContent(D data) {
+    errorView.setVisibility(GONE);
+    emptyView.setVisibility(GONE);
+    progressView.setVisibility(GONE);
+  }
 
   @Override
-  public void showEmpty() {}
+  public void showEmpty() {
+    errorView.setVisibility(GONE);
+    emptyView.setVisibility(VISIBLE);
+    progressView.setVisibility(GONE);
+  }
 
   @Override
-  public void showError(String message) {}
+  public void showError(String message) {
+    errorView.setVisibility(VISIBLE);
+    emptyView.setVisibility(View.GONE);
+    progressView.setVisibility(GONE);
+  }
 
   @Override
-  public void showLoading() {}
+  public void showLoading() {
+    errorView.setVisibility(View.GONE);
+    emptyView.setVisibility(View.GONE);
+    progressView.setVisibility(VISIBLE);
+  }
 }
