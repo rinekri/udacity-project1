b32bb537f8e73ac3bcaf18ba469e2d37b4933bbc
ilya <rinekri@gmail.com>
2017-04-12 06:54:06 +0200

    Update Enum logic to store String res

--- app/src/main/java/ru/rinekri/udacitypopularmovies/ui/base/MovieSortType.java
+++ app/src/main/java/ru/rinekri/udacitypopularmovies/ui/base/MovieSortType.java
@@ -1,3 +1,20 @@
 package ru.rinekri.udacitypopularmovies.ui.base;
 
-public enum MovieSortType { Popular, TopRated }
+import android.support.annotation.StringRes;
+
+import ru.rinekri.udacitypopularmovies.R;
+
+public enum MovieSortType {
+  Popular(R.string.main_sort_popular_movies),
+  TopRated(R.string.main_sort_top_rated_movies);
+
+  MovieSortType(@StringRes Integer nameRes) {
+    this.nameRes = nameRes;
+  }
+
+  private Integer nameRes;
+
+  public Integer getNameRes() {
+    return nameRes;
+  }
+}
