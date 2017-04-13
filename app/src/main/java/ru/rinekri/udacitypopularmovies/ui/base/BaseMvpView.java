c67727fdeb531db0c9fb3df8e00f4ca2552376f1
ilya <rinekri@gmail.com>
2017-04-13 21:40:51 +0200

    Add StateStrategy

--- app/src/main/java/ru/rinekri/udacitypopularmovies/ui/base/BaseMvpView.java
+++ app/src/main/java/ru/rinekri/udacitypopularmovies/ui/base/BaseMvpView.java
@@ -5,9 +5,12 @@ import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 
 import com.arellomobile.mvp.MvpView;
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 
 import ru.rinekri.udacitypopularmovies.ui.base.models.ErrorConfig;
 
+@StateStrategyType(AddToEndSingleStrategy.class)
 public interface BaseMvpView<D> extends MvpView {
   void showViewContent(D data);
   void showLoading();
